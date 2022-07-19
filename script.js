ab82db313972c77c6fec5a09fe91a2b7fe2bef70
Infinityworks <107305997+DiamondPixel4k@users.noreply.github.com>
2022-07-19 11:36:44 +0100

    Upload files

---
+++ script.js
@@ -0,0 +1,27 @@
+function megamindJumpscare() {
+  document.getElementById("no4").src = "megamind.jpeg";
+}
+
+function flashJumpscare() {
+  document.getElementById("body").classList.add("flash");
+  setTimeout(function(){
+    document.getElementById("body").classList.remove("flash");
+  }, 1000);
+}
+
+function growTitle() {
+  document.getElementById("title").classList.add("grow");
+  setTimeout(function(){
+    document.getElementById("title").classList.remove("grow");
+  }, 5000);
+}
+
+async function ip() {
+  let response = await fetch('https://api.ipify.org/');
+  if (response.ok) {
+    let ip = await response.text();
+    document.getElementById("ip").innerHTML = ip;
+    document.getElementById("no1").style.visibility = "hidden";
+    document.getElementById("ip").classList.add("zoom");
+  }
+}
